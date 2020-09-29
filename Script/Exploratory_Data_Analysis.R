135a2ccfec2a3374af3bafbeebaee17e1d62ff2d
japellaniz <jose.apellaniz@gmail.com>
2020-09-29 19:31:11 +0200

    gitignore

---
+++ Script/Exploratory_Data_Analysis.R
@@ -0,0 +1,9 @@
+
+library(data.table)
+
+df_clientes <- read.csv("Data/cliente_tabla.csv")
+df_producto <- read.csv("Data/producto_tabla.csv")
+df_town_state <- read.csv("Data/town_state.csv")
+df_train <- fread("Data/train.csv")
+
+
